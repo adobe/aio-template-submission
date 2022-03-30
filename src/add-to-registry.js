89fabe07394a0dba9b293a01a58dabedc1832b37
lenaorobei <oorobei@adobe.com>
2022-03-30 11:01:56 -0500

    DEVX-2072: Registry handlers

--- src/add-to-registry.js
+++ src/add-to-registry.js
@@ -1,6 +1,7 @@
 import fs from 'fs' ;
 import {v4 as uuidv4} from 'uuid';
 import fetch from 'node-fetch';
+import * as core from '@actions/core';
 
 // Simple script that collects template metadata and adds it to the registry
 const myArgs = process.argv.slice(2);
@@ -12,10 +13,13 @@ const repo = gitHubUrl.split('/').slice(-2).join('/');
 const gitHubResponse = (async () => {
     await fetch('https://api.github.com/repos/' + repo);
 })();
+
+// We expect repo to have more than 10 stargazers in order to be featured
+const stargazersThreshold = 10;
 const stargazersCount = (async () => {
     await gitHubResponse.text()['stargazers_count'];
 })();
-
+const adobeRecommended = stargazersCount > stargazersThreshold;
 
 // Create registry item object
 const registryItem = {
@@ -34,7 +38,7 @@ const registryItem = {
         "aio-action",
         "aio-graphql"
     ],
-    "adobeRecommended": stargazersCount > 10,
+    "adobeRecommended": adobeRecommended,
     "keywords": packageData.keywords,
     "links": {
         "npm": myArgs[2],
@@ -45,13 +49,18 @@ const registryItem = {
 // Check for duplicates
 const registry = JSON.parse(fs.readFileSync('registry.json'));
 if (registry.filter(e => e.name === registryItem.name).length > 0) {
-    throw new Error(':x: Template with name `' + registryItem.name + '` already exists in Template Registry.')
+    const errorMessage = ':x: Template with name `' + registryItem.name + '` already exists in Template Registry.';
+    core.setOutput('error', errorMessage);
+    throw new Error(errorMessage)
 }
 
 // Add to the registry
 registry.push(registryItem);
 const newData = JSON.stringify(registry, null, "  ");
 fs.writeFile('registry.json', newData, err => {
-    if (err) throw err;
+    if (err) {
+        core.setOutput('error', ':warning: Error occurred during adding template to the Template Registry.');
+        throw err;
+    }
     console.log('Template was added', newData);
 });
