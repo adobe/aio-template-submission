932745a0a6a203ca3fdea6809bfdb898fe7bdfea
Serhii Litviachenko <slitviachenko@gmail.com>
2022-04-04 09:34:45 -0500

    DEVX-2113: Integrate validation library with GitHub actions

--- src/check-npm-package-metadata.js
+++ src/check-npm-package-metadata.js
@@ -1,21 +1,23 @@
 import * as core from '@actions/core';
 import * as metadata from '@adobe/aio-lib-template-validation';
 
-const myArgs = process.argv.slice(2);
-const packageName = myArgs[0];
-try {
-  var results = await metadata.checkTemplateMetadata(packageName);
-} catch (e) {
-  core.setOutput('error', `:x: ${e.message}`);
-  throw e;
-}
-
-if (results.stats.failures !== 0) {
-  let errors = [];
-  for (const test of results.failures) {
-    errors.push(`:x: ${test.message}`);
+(async () => {
+  const myArgs = process.argv.slice(2);
+  const packageName = myArgs[0];
+  try {
+    var results = await metadata.checkTemplateMetadata(packageName);
+  } catch (e) {
+    core.setOutput('error', `:x: ${e.message}`);
+    throw e;
   }
-  let errorMessage = errors.join('\n');
-  core.setOutput('error', `${errorMessage}`);
-  throw new Error(errorMessage);
-}
+
+  if (results.stats.failures !== 0) {
+    let errors = [];
+    for (const test of results.failures) {
+      errors.push(`:x: ${test.message}`);
+    }
+    let errorMessage = errors.join('\n');
+    core.setOutput('error', `${errorMessage}`);
+    throw new Error(errorMessage);
+  }
+})();
