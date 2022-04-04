ce465543ba312622a6ce5d4415bb3d238c2a96aa
Serhii Litviachenko <slitviachenko@gmail.com>
2022-04-04 12:50:27 -0500

    DEVX-2113: Integrate validation library with GitHub actions (#66)
    DEVX-2113: Integrate validation library with GitHub actions

---
+++ src/check-npm-package-metadata.js
@@ -0,0 +1,23 @@
+import * as core from '@actions/core';
+import * as metadata from '@adobe/aio-lib-template-validation';
+
+(async () => {
+  const myArgs = process.argv.slice(2);
+  const packageName = myArgs[0];
+  try {
+    var results = await metadata.checkTemplateMetadata(packageName);
+  } catch (e) {
+    core.setOutput('error', `:x: ${e.message}`);
+    throw e;
+  }
+
+  if (results.stats.failures !== 0) {
+    let errors = [];
+    for (const test of results.failures) {
+      errors.push(`:x: ${test.message}`);
+    }
+    let errorMessage = errors.join('\n');
+    core.setOutput('error', `${errorMessage}`);
+    throw new Error(errorMessage);
+  }
+})();
