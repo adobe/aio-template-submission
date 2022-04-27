880cad65ee7a634eb1c6782bd7345920a1c67eb2
Serhii Litviachenko <slitviachenko@gmail.com>
2022-04-27 16:01:15 -0500

    DEVX-2221: Update workflows to reflect API changes - added `npm test` to CI.yml

--- tests/validate-json-schema.test.js
+++ tests/validate-json-schema.test.js
@@ -1,7 +1,7 @@
 const { expect, describe, test } = require('@jest/globals');
-const { v4: uuidv4 } = require('uuid');
 const core = require('@actions/core');
 const validateJson = require('../src/validate-json-schema');
+const { generateRegistryItem } = require('./helper');
 
 jest.mock('@actions/core');
 
@@ -19,7 +19,7 @@ describe('Verify "Template Registry" JSON Schema validation', () => {
         let registryItems = [];
         registryItems.push(generateRegistryItem('@adobe/app-builder-template-1'));
         let registryItem = generateRegistryItem('@adobe/app-builder-template-2');
-        delete registryItem.description;
+        delete registryItem.name;
         registryItems.push(registryItem);
 
         expect(() => validateJson(JSON.stringify(registryItems, null, 4)))
@@ -27,36 +27,3 @@ describe('Verify "Template Registry" JSON Schema validation', () => {
         expect(core.setOutput).toHaveBeenCalledWith('error', ':warning: Attempt to save invalid data to the registry. See `registry.schema.json` for more details.');
     });
 });
-
-function generateRegistryItem(templateName) {
-    const registryItem = {
-        'id': uuidv4(),
-        'author': 'Adobe Inc.',
-        'name': templateName,
-        'description': 'A template for testing purposes',
-        'latestVersion': '1.0.1',
-        'publishDate': (new Date(Date.now())).toISOString(),
-        'extensions': [
-            'dx/excshell/1'
-        ],
-        'categories': [
-            'add-action'
-        ],
-        'services': [
-            'AnalyticsSDK',
-            'CampaignStandard',
-            'Runtime'
-        ],
-        'adobeRecommended': true,
-        'keywords': [
-            'aio',
-            'adobeio',
-            'aio-app-builder-template'
-        ],
-        'links': {
-            'npm': `https://www.npmjs.com/package/${templateName}`,
-            'github': `https://github.com/${templateName}`
-        }
-    }
-    return registryItem;
-}
