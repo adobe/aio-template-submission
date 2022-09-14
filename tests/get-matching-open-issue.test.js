c494a02225ceb6440117d11c4a8f62cefaf95aaa
Amy Zhao <azhao@adobe.net>
2022-09-14 14:32:43 -0500

    add tests

--- tests/get-matching-open-issue.test.js
+++ tests/get-matching-open-issue.test.js
@@ -47,4 +47,57 @@ describe('Get matching open issue of approved template', () => {
         await expect(getTemplateGithubIssues(label, npmPackage, githubRepoOwner, githubRepo))
             .resolves.toStrictEqual(response[0]);
     });
+  test('No matching open issue', async () => {
+    const response = [{
+      "body": "### npm package name\n\n@ajzhao32423423894/app-builder-template",
+      "html_url": "https://github.com/devx-services/aio-template-submission/issues/81",
+      "number": 81
+    },
+    {
+      "body": "### npm package name\n\n@adobe/template-1",
+      "html_url": "https://github.com/devx-services/aio-template-submission/issues/81",
+      "number": 83
+    },
+    {
+      "body": "### npm package name\n\n@adobe/template-2",
+      "html_url": "https://github.com/devx-services/aio-template-submission/issues/81",
+      "number": 84
+    }]
+    const label = 'update-template';
+    const npmPackage = '@adobe/template-3';
+    const githubRepoOwner = 'adobe'
+    const githubRepo = 'aio-template-submission'
+    nock('https://api.github.com')
+      .get(`/repos/${githubRepoOwner}/${githubRepo}/issues?state=open&labels=${label}`)
+      .times(1)
+      .reply(200, response);
+    await expect(getTemplateGithubIssues(label, npmPackage, githubRepoOwner, githubRepo))
+        .resolves.toStrictEqual({});
+  });
+
+  test('No open issues', async () => {
+    const label = 'update-template';
+    const npmPackage = '@adobe/template-3';
+    const githubRepoOwner = 'adobe'
+    const githubRepo = 'aio-template-submission'
+    nock('https://api.github.com')
+      .get(`/repos/${githubRepoOwner}/${githubRepo}/issues?state=open&labels=${label}`)
+      .times(1)
+      .reply(200, []);
+    await expect(getTemplateGithubIssues(label, npmPackage, githubRepoOwner, githubRepo))
+        .resolves.toStrictEqual({});
+  });
+
+  test('Error handling', async () => {
+    const label = 'update-template';
+    const npmPackage = '@adobe/template-3';
+    const githubRepoOwner = 'adobe'
+    const githubRepo = 'aio-template-submission'
+    nock('https://api.github.com')
+      .get(`/repos/${githubRepoOwner}/${githubRepo}/issues?state=open&labels=${label}`)
+      .times(1)
+      .reply(404);
+    await expect(getTemplateGithubIssues(label, npmPackage, githubRepoOwner, githubRepo))
+        .resolves.toHaveProperty("status", 404)
+  });
 });
